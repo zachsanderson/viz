c2c4bcdba4b52e7fbb508da045bb26ff02dfe8ab
Zach Sanderson <zach@zachsanderson.com>
2016-04-02 16:02:55 -0500

    Stepping through nested array with divs

--- bestrecord/script.js
+++ bestrecord/script.js
@@ -1,7 +1,20 @@
 // var seasondata = [];
 
-function doSomethingWithData(jsondata) {
-	console.log(jsondata);
-}
+d3.json('seasons.json', function(json) {
+    console.log(json);
+    //console.log(json.seasons[1].standings[5]);
+    console.log(d3.keys(json));
+    console.log(d3.values(json[0]));
+    var seasondiv = d3.select("#chart").selectAll("div.season")
+						.data(json)
+						.enter()
+						.append("div")
+						.attr("class","season")
+						.html(function (d) { return d.season + '<br>' + d.winners_place; });
+	seasondiv.selectAll("div")
+		.data(function(d) {return d.standings})
+		.enter()
+		.append("div").attr("class","team")
+		.html(function (d) { return d.team});
 
-d3.json("seasons.json", doSomethingWithData);
+});
